9f7110c5b5eb782ebe9e3e132ac1ad69d4d357f0
Fábián Álmos <fabian.almos77@gmail.com>
2026-04-11 22:54:37 +0200

    Added logging

--- handler/handle.go
+++ handler/handle.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/FabianAlmos/contfuncs-sdk/fn_http"
@@ -14,6 +15,8 @@ func Handle[In any, Out any](fn Handler[In, Out]) error {
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Handling request...")
+
 		var req fn_http.Request[In]
 		if r.ContentLength > 0 {
 			if err := json.NewDecoder(r.Body).Decode(&req.Payload); err != nil {
@@ -33,6 +36,6 @@ func Handle[In any, Out any](fn Handler[In, Out]) error {
 		json.NewEncoder(w).Encode(resp)
 	})
 
-	fmt.Printf("Function listening on port: %q\n", port)
+	log.Printf("Function listening on port: %q\n", port)
 	return http.ListenAndServe(":"+port, nil)
 }
